78e4b6e5f1f7aa47d3165950dc3992e0e315da2d
Daniel Olvera <ing.josuedanielescamilla@gmail.com>
2024-10-09 10:12:36 -0700

    Composable to create dialog for runtime permissions

--- app/src/main/java/com/danielolvera/weatherappcompose/home/view/RequestLocationPermission.kt
+++ app/src/main/java/com/danielolvera/weatherappcompose/home/view/RequestLocationPermission.kt
@@ -1,28 +1,45 @@
 package com.danielolvera.weatherappcompose.home.view
 
+import android.Manifest
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun RequestLocationPermission(
-    onPermissionGranted: () -> Unit
-) {
-    val finePermissionState = rememberPermissionState(android.Manifest.permission.ACCESS_FINE_LOCATION)
+fun RequestLocationPermission(onPermissionGranted: () -> Unit) {
+    val finePermissionState = rememberPermissionState(Manifest.permission.ACCESS_FINE_LOCATION)
+    val coarsePermissionState = rememberPermissionState(Manifest.permission.ACCESS_COARSE_LOCATION)
 
-    if (finePermissionState.status.isGranted) {
+    // Check if both permissions are granted
+    if (finePermissionState.status.isGranted && coarsePermissionState.status.isGranted) {
         onPermissionGranted()
     } else {
-        Column {
-            Text("Location permission is required to get weather by location.")
-            Button(onClick = { finePermissionState.launchPermissionRequest() }) {
-                Text("Grant Permission")
+        LaunchedEffect(finePermissionState.status, coarsePermissionState.status) {
+            if (!finePermissionState.status.isGranted) {
+                finePermissionState.launchPermissionRequest()
             }
+            if (!coarsePermissionState.status.isGranted) {
+                coarsePermissionState.launchPermissionRequest()
+            }
+        }
+
+        // To inform the user if they refused permissions
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Location permissions are required to fetch weather data by your current location.")
         }
     }
 }
+
