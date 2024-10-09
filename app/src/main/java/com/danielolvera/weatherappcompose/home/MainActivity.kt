a1bbbb481183d6bffd1ed8ffd65f50fb47f8623b
Daniel Olvera <ing.josuedanielescamilla@gmail.com>
2024-10-09 10:27:48 -0700

    Added request for user runtime permission and covered edge cases. Also added a fallback in case of location failing to use the latest saved city.

--- app/src/main/java/com/danielolvera/weatherappcompose/home/MainActivity.kt
+++ app/src/main/java/com/danielolvera/weatherappcompose/home/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.danielolvera.weatherappcompose.home
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +13,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import com.danielolvera.weatherappcompose.BuildConfig
 import com.danielolvera.weatherappcompose.home.intent.WeatherIntent
@@ -19,10 +24,18 @@ import com.danielolvera.weatherappcompose.home.view.WeatherScreen
 import com.danielolvera.weatherappcompose.home.view.utils.SharedPreferencesManager
 import com.danielolvera.weatherappcompose.home.viewmodel.ViewModelFactory
 import com.danielolvera.weatherappcompose.home.viewmodel.WeatherViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var weatherViewModel: WeatherViewModel
+    private lateinit var fusedLocation: FusedLocationProviderClient
+    private var isWeatherReceived = false
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
@@ -36,6 +49,7 @@ class MainActivity : ComponentActivity() {
         val factory = ViewModelFactory { WeatherViewModel(repository, apiKey) }
 
         weatherViewModel = ViewModelProvider(this, factory)[WeatherViewModel::class.java]
+        fusedLocation =LocationServices.getFusedLocationProviderClient(this)
 
         setContent {
 
@@ -53,6 +67,43 @@ class MainActivity : ComponentActivity() {
         val sharedPreference = SharedPreferencesManager(this)
         sharedPreference.getLastCity()?.let { lastCity ->
             weatherViewModel.handleIntent(WeatherIntent.FetchWeatherData(lastCity))
+        }
+    }
+
+    fun requestUserLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocation.lastLocation.addOnSuccessListener { location: Location? ->
+                location?.let {
+                    if (!isWeatherReceived) {
+                        val latitude = it.latitude
+                        val longitude = it.longitude
+
+                        weatherViewModel.handleIntent(WeatherIntent.FetchWeatherByLocation(latitude, longitude))
+                        isWeatherReceived = true
+                    }
+                } ?: run {
+                    Log.e(TAG, "Location is null, fetching last searched city.")
+
+
+                    fallbackToLastSearchedCity()
+                }
+            }.addOnFailureListener { e ->
+                Log.e(TAG, "Failed to get location: ${e.message}")
+                fallbackToLastSearchedCity()
+            }
+        } else {
+            Log.e(TAG, "Location permissions are not granted")
+            fallbackToLastSearchedCity()
+        }
+    }
+
+    private fun fallbackToLastSearchedCity() {
+        val lastCity = SharedPreferencesManager(this).getLastCity()
+        if (lastCity != null) {
+            weatherViewModel.handleIntent(WeatherIntent.FetchWeatherData(lastCity))
+        } else {
+            Log.e(TAG, "There is no last searched city available.")
         }
     }
 }
