22a053c1e8192067f6c4c9607b7930f0f11e8299
Daniel Olvera <ing.josuedanielescamilla@gmail.com>
2024-10-08 20:57:55 -0700

    Added repository

---
+++ app/src/main/java/com/danielolvera/weatherappcompose/home/model/repo/WeatherRepository.kt
@@ -0,0 +1,21 @@
+package com.danielolvera.weatherappcompose.home.model.repo
+
+import com.danielolvera.weatherappcompose.home.model.network.WeatherApiService
+import com.danielolvera.weatherappcompose.home.model.state.WeatherState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class WeatherRepository (private val weatherApiService: WeatherApiService) {
+
+    fun getWeather(city: String, apiKey: String): Flow<WeatherState> = flow {
+        emit(WeatherState.Loading)
+        try {
+            val response = weatherApiService.getWeatherByCity(city, apiKey)
+            emit(WeatherState.Success(response))
+        } catch (e: Exception) {
+            emit(WeatherState.Error(e.message ?: "Unknown error. How?"))
+        }
+    }.flowOn(Dispatchers.IO)
+}
